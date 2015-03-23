520bb7bf87eb0c5502be0981e91c44e57d9befc6
Stanislav Usenkov <SimSonic@users.noreply.github.com>
2015-03-23 18:56:36 +0600

    Добавлена заглушка для класса com.mojang.authlib.properties.Property

---
+++ src/main/java/com/mojang/authlib/properties/Property.java
@@ -0,0 +1,44 @@
+package com.mojang.authlib.properties;
+import java.lang.reflect.InvocationTargetException;
+import java.security.PublicKey;
+
+public class Property
+{
+	private final String name;
+	private final String value;
+	private final String signature;
+	public Property(String value, String name)
+	{
+		this(value, name, null);
+	}
+	public Property(String name, String value, String signature)
+	{
+		this.name      = name;
+		this.value     = value;
+		this.signature = signature;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getValue()
+	{
+		return value;
+	}
+	public String getSignature()
+	{
+		return signature;
+	}
+	public boolean hasSignature()
+	{
+		return signature != null;
+	}
+	public boolean isSignatureValid(PublicKey publicKey)
+	{
+		return true;
+	}
+	public static boolean isFake() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException
+	{
+		return true;
+	}
+}
