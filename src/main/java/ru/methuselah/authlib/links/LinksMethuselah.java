6a0bde5e412bb42a48bfb6bee7654c7770876bde
SimSonic <simsonic@yandex.ru>
2015-11-03 16:37:35 +0600

    Fix own methuselah links.

--- src/main/java/ru/methuselah/authlib/links/LinksMethuselah.java
+++ src/main/java/ru/methuselah/authlib/links/LinksMethuselah.java
@@ -14,10 +14,10 @@ public final class LinksMethuselah extends Links
 			"/hasJoined.php",
 			"/legacy/legacy_join.php",
 			"/legacy/legacy_check.php",
-			"/api/getuuid.php",
-			"/api/namehistory.php",
+			"/api/getuuid.php?name=",
+			"/api/namehistory.php?uuid=",
 			"/api/namestouuids.php",
-			"/api/clothes.php");
+			"/api/clothes.php?uuid=");
 		super.setProvider(Links.LinksProvider.methuselah);
 	}
 }
